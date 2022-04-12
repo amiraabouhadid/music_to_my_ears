2e72eac0ebf1035833d68ce365d36c70b2570282
amira <amiraabouhadid@gmail.com>
2022-04-12 18:07:48 +0200

    update tracks to link to track preview download

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,19 +3,21 @@ import { BsArrowRightCircle } from 'react-icons/bs';
 
 const Track = ({ track, idx }) => (
   <div className={idx % 2 !== 0 ? 'dark-bg p-2' : 'p-2'}>
-    <div className="text-light pt-3 d-flex flex-row align-items-baseline justify-content-between">
-      <p className="fw-bold">{track.name}</p>
+
+    <a
+      href={track.previewUrl}
+      style={{ textDecoration: 'none' }}
+      className="text-light pt-3 d-flex flex-row flex-nowrap align-items-baseline justify-content-between"
+    >
+      <p className="fw-bold" style={{ fontSize: '0.85rem' }}>{track.name.toUpperCase()}</p>
 
       <p>
         {`${track.playbackSeconds} seconds`}
-        <a href={track.previewUrl} className="mx-2 text-light">
-          <BsArrowRightCircle />
 
-        </a>
+        <BsArrowRightCircle className="mx-2 text-light" />
 
       </p>
-
-    </div>
+    </a>
 
   </div>
 
